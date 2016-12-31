bba78f76a5d7fdd05eb45885d5cd991fb4c9b6f9
szasanyi <debeloksanyi@gmail.com>
2016-12-31 13:35:05 +0200

    Split main controller to Thermostat/User controller

--- tempr-backend-api/src/main/java/edu/endava/tempr/api/controller/ThermostatController.java
+++ tempr-backend-api/src/main/java/edu/endava/tempr/api/controller/ThermostatController.java
@@ -1,12 +1,10 @@
 package edu.endava.tempr.api.controller;
 
 import edu.endava.tempr.api.assembler.ThermostatAssembler;
-import edu.endava.tempr.api.assembler.UserAssembler;
 import edu.endava.tempr.api.service.ThermostatService;
 import edu.endava.tempr.api.service.UserService;
 import edu.endava.tempr.common.ThermostatDto;
 import edu.endava.tempr.common.ThermostatLogDto;
-import edu.endava.tempr.common.UserDto;
 import edu.endava.tempr.model.Thermostat;
 import edu.endava.tempr.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,55 +13,20 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+/**
+ * Created by zsoltszabo on 31/12/2016.
+ */
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-public class MyRestController {
-
-    @Autowired
-    private UserService userService;
-
+public class ThermostatController {
     @Autowired
     private ThermostatService thermostatService;
 
     @Autowired
-    private UserAssembler userAssembler;
-
-    @Autowired
     private ThermostatAssembler thermostatAssembler;
 
-    @RequestMapping(value = "/user/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<UserDto>> getAllUsers() {
-        List<User> users = userService.findAll();
-        List<UserDto> userDtoList = users.stream().map(user -> userAssembler.toDto(user)).collect(Collectors.toList());
-        return new ResponseEntity<>(userDtoList, HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/user/{id}/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
-        User user = userService.findOne(id);
-
-        // If no user found with the given id
-        if (user == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(userAssembler.toDto(user), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/user/login/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDto> loginUser() {
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/user/register/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userDto) {
-        User user = userAssembler.toEntity(userDto);
-
-        //Based on whether the user was created or not
-        return (userService.createUser(user) != null) ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/thermostat/register/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ThermostatDto> registerThermostat(@RequestBody ThermostatDto thermostatDto) {
@@ -102,5 +65,4 @@ public class MyRestController {
         System.out.println("INFO extTemp" + thermostatLogDto.getExtTemp());
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
