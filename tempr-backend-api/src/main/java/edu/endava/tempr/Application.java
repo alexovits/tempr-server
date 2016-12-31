574e59b1f99d359276df4776dce5bc54569b1526
szasanyi <debeloksanyi@gmail.com>
2016-12-31 15:56:13 +0200

    Set up log4j2+slf4j logging

--- tempr-backend-api/src/main/java/edu/endava/tempr/Application.java
+++ tempr-backend-api/src/main/java/edu/endava/tempr/Application.java
@@ -6,9 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
@@ -18,6 +22,9 @@ public class Application {
     public CommandLineRunner addDefaultUser(UserService userService) {
         return (args) -> {
             userService.createUser(new User("user","user","John","Doe","user@tempr.com"));
+            logger.info("TEMPLATE INFO--------------------!!!");
+            logger.debug("TEMPLATE DEBUG--------------------!!!");
+            logger.error("TEMPLATE ERROR--------------------!!!");
         };
     }
 }
