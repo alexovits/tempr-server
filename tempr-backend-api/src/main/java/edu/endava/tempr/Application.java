4ee39d699abbe3b5d136dbf5fa136c69bf57690d
alexovits <debeloksanyi@gmail.com>
2017-03-16 15:00:53 +0200

    Add Swagger support (+UI)

--- tempr-backend-api/src/main/java/edu/endava/tempr/Application.java
+++ tempr-backend-api/src/main/java/edu/endava/tempr/Application.java
@@ -16,6 +16,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 import java.util.Random;
