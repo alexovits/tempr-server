e3cd4d447605b84d6d20986dfa6ebc10b016782a
alexovits <debeloksanyi@gmail.com>
2017-04-09 22:06:30 +0300

    Base sim service

---
+++ tempr-backend-api/src/main/java/edu/endava/tempr/api/service/SimulatorService.java
@@ -0,0 +1,12 @@
+package edu.endava.tempr.api.service;
+
+/**
+ * Created by zsoltszabo on 4/9/17.
+ */
+public interface SimulatorService {
+    void addHeatingCircuit(long chipId);
+    void removeHeatingCircuit(long chipId);
+    void logTemperature(int temperature, long chipId);
+    void setSuggestedTemperature(int temperature, long chipId);
+    void setDesiredTemperature(int temperature, long chipId);
+}
