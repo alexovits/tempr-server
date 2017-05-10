d1635fe3723ae3205b49f51d754e91715fd70baf
alexovits <debeloksanyi@gmail.com>
2017-05-10 19:23:58 +0300

    Add index to sensorLog's heatingCircuit-ID

--- tempr-backend-api/src/main/java/edu/endava/tempr/api/service/SuggestionService.java
+++ tempr-backend-api/src/main/java/edu/endava/tempr/api/service/SuggestionService.java
@@ -1,8 +1,11 @@
 package edu.endava.tempr.api.service;
 
+import java.util.List;
+
 /**
  * Created by zsoltszabo on 5/10/17.
  */
 public interface SuggestionService {
-    Double calculateSuggestionInterface(int heatingCircuitId, );
+    Double getSuggestionTemperature(int day, int hour) throws Exception;
+    List<Double> getSuggestionTemperature(int day) throws Exception;
 }
