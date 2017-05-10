1ac59ebf93434987abde977ba0cee5f3ebedfb08
alexovits <debeloksanyi@gmail.com>
2017-05-10 19:09:17 +0300

    Add base ai classes

---
+++ tempr-backend-api/src/main/java/edu/endava/tempr/api/service/SuggestionService.java
@@ -0,0 +1,8 @@
+package edu.endava.tempr.api.service;
+
+/**
+ * Created by zsoltszabo on 5/10/17.
+ */
+public interface SuggestionService {
+    Double calculateSuggestionInterface(int heatingCircuitId, );
+}
---
+++ tempr-backend-api/src/main/java/edu/endava/tempr/api/service/impl/ai/SuggestionService.java
@@ -0,0 +1,8 @@
+package edu.endava.tempr.api.service.impl.ai;
+
+/**
+ * Created by zsoltszabo on 5/10/17.
+ */
+public class SuggestionService {
+
+}
